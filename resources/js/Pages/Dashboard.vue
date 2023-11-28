161a1935d79a701d66403ceff2ac57d0bdb74437
curder <q.curder@gmail.com>
2023-11-28 11:03:53 +0800

    Setting up state and selecting a file

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,13 +1,32 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import {Head} from '@inertiajs/vue3';
+import {reactive, ref} from "vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+
 defineProps({
     files: Array,
 })
+
+const file = ref(null)
+
+const state = reactive({
+    file: null,
+})
+
+const submit = () => {
+    state.file = file.value.files[0];
+
+    if (!state.file) {
+        return
+    }
+
+    console.log('start uploaded');
+}
 </script>
 
 <template>
-    <Head title="Dashboard" />
+    <Head title="Dashboard"/>
 
     <AuthenticatedLayout>
         <template #header>
@@ -15,7 +34,18 @@ defineProps({
         </template>
 
         <div class="py-12">
-            <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 space-y-4">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <form class="p-6 text-gray-900" @submit.prevent="submit">
+
+                        <div class="flex items-center">
+                            <input type="file" name="file" ref="file" class="flex-grow">
+                            <PrimaryButton>Upload</PrimaryButton>
+                        </div>
+
+                    </form>
+                </div>
+
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
                         <template v-if="files.length">
