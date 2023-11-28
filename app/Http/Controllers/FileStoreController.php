f22ef5a5006e818fa04c2957d126a2265b24b477
curder <q.curder@gmail.com>
2023-11-28 11:36:19 +0800

    Handling chunks on the server

--- app/Http/Controllers/FileStoreController.php
+++ app/Http/Controllers/FileStoreController.php
@@ -3,11 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Pion\Laravel\ChunkUpload\Handler\ContentRangeUploadHandler;
+use Pion\Laravel\ChunkUpload\Receiver\FileReceiver;
+use Str;
+use function dd;
 
 class FileStoreController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
+        $receiver = new FileReceiver(
+            UploadedFile::fake()->createWithContent('file', $request->getContent()),
+            $request,
+            ContentRangeUploadHandler::class
+        );
 
+        $save = $receiver->receive();
+
+        if ($save->isFinished()) {
+            $this->storeFile($request, $save->getFile());
+        }
+
+        $save->handler();
+    }
+
+    private function storeFile(Request $request, UploadedFile $file)
+    {
+        $extension = $file->guessExtension();
+
+        $request->user()->files()->create([
+           'file_path' => $file->storeAs('files', Str::uuid() . '.' . $extension, 'public'),
+        ]);
     }
 }
--- app/Models/File.php
+++ app/Models/File.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'file_path'
+    ];
 }
